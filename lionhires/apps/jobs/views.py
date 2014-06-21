563c58ea3b99353fd4b61b473e700771cc03732c
Matthew Piccolella <mjp2220@columbia.edu>
2014-06-21 16:03:41 +0000

    Finishing this up here

--- lionhires/apps/jobs/views.py
+++ lionhires/apps/jobs/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def home(request):
-  c = {}
-  return render_to_response('index.html', c)
+  data = request.GET.copy();
+  if 'code' in data:
+    return HttpResponse(data['code'])
+  else:
+    return render_to_response('index.html', c)
