e4e9ac130d7549b35a964fb65629b9cbf861b58d
Matthew Piccolella <mjp2220@columbia.edu>
2014-06-21 16:21:31 +0000

    Trying to get the authentication working

--- lionhires/apps/jobs/views.py
+++ lionhires/apps/jobs/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from linkedin import linkedin
+
+LINKED_IN_API_KEY = '77xwhoqz5szdvj'
+LINKED_IN_SECRET = 'C2bnhwPZIKMTFIby'
 
 def home(request):
   data = request.GET.copy();
   if 'code' in data:
-    return HttpResponse(data['code'])
+    return HttpResponse("Redirect works!")
   else:
-    return render_to_response('index.html', c)
+    authentication = linkedin.LinkedInAuthorization(API_KEY, API_SECRET, request.build_absolute_uri(), linkedin.PERMISSIONS.enums.values())
+    return HttpResponse(authentication.authorization_url)    
+    
